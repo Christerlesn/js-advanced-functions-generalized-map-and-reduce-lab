8d40d94fcb91b65b15d84eb044f40461d30d9291
Christerle_Nounoute <christerlesn@gmail.com>
2021-08-14 10:42:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(src, funcOper) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(funcOper(src[i])) // Unique work
+    }
+    return r
+  }
+
+  function reduce(origArr, funcOper, startPt){
+      let total;
+      let i;
+
+      if (!!startPt){
+        total = startPt
+        i = 0
+      }else{
+          total = origArr[0];
+          i = 1
+      }
+    for( ; i < origArr.length; i++) {
+        total = funcOper(origArr[i], total);
+    };
+    return total;
+  }
